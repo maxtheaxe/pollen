65a0093d0603f5b93c43f031689bd4f1c6df6251
maxtheaxe <max.perrello@gmail.com>
2021-04-15 21:24:44 -0400

    removed unnecessary data from message box screen obj

--- main.py
+++ main.py
@@ -39,14 +39,19 @@ else:
 class ImageButton(ButtonBehavior, Image):
 	'''image that can also be clicked like a button, with iname property as well'''
 	# ref: https://stackoverflow.com/a/33489264
+	pass
+
 class NamedImageButton(ImageButton):
 	'''image that can also be clicked like a button, with iname property as well'''
 	def __init__(self, iname, **kwargs):
 		super(ImageButton, self).__init__(**kwargs)
 		self.iname = iname
+	pass
+
 class BoxButton(ButtonBehavior, BoxLayout):
 	'''a box layout that also allows onpress events'''
 	pass
+
 class ConvoItem(BoxButton):
 	'''a mini preview version of a conversation'''
 	# contact_name = StringProperty('') # blank string is default # review: does this NEED to be a kivy prop?
@@ -73,34 +78,30 @@ class ConvoItem(BoxButton):
 		# MessageBox.set_contact()
 		# ConversationScreen.set_contact(chosen_contact) # pass contact key string
 	pass
+
 class Message(BoxButton):
 	'''parent message type'''
 	message_text = StringProperty('') # blank string is default
 	sent = BooleanProperty(True) # blank string is default
 	pass
+
 class ClearMessage(Message):
 	'''plaintext view of message'''
 	pass
+
 class InMessage(Message):
 	'''inbox view of message'''
 	pass
+
 class OutMessage(Message):
 	'''outbox view of message'''
 	pass
+
 class HeaderBar(BoxLayout):
 	'''box that contains a back button and screen title'''
 	title = StringProperty('') # blank string is default
 	pass
-# class ContentBox(RecycleView):
-# 	'''box that contains a scrollable section of content (either messages or conversations)'''
-# 	# ref: https://www.geeksforgeeks.org/python-recycleview-in-kivy/
-# 	def __init__(self, **kwargs): 
-# 		super(ContentBox, self).__init__(**kwargs)
-# 		self.data = []
-# 		global client_instance
-# 		for contact in client_instance.conversation_manager.conversations:
-# 			friendly_name = client_instance.conversation_manager.conversations[contact].friendly_name
-# 			self.data.append({'contact_name': friendly_name, 'contact': contact})
+
 class ConvoBox(RecycleView):
 	'''box that contains a scrollable section of content (either messages or conversations)'''
 	# ref: https://www.geeksforgeeks.org/python-recycleview-in-kivy/
@@ -113,58 +114,46 @@ class ConvoBox(RecycleView):
 			for contact in client_instance.conversation_manager.conversations:
 				friendly_name = client_instance.conversation_manager.conversations[contact].friendly_name
 				self.data.append({'contact_name': friendly_name, 'contact': contact})
+	pass
+
 class MessageBoxHeader(BoxLayout):
 	pass
+
 class MessageBox(RecycleView):
 	'''basic building block for message boxes'''
 	# if i want to use this widget elsewhere and pass in a property at instantiation
 	# need to define a kivy property as such:
 	title = StringProperty('') # blank string is default
-	# contact_key = StringProperty('') # key for selecting contact (check: does this NEED to be a kivy prop?)
-	# (should be a conversation later)
-	# by default, it doesn't wipe old value--prob need to add func to do so,
-	# but dynamic updating isn't necessary here
-	def __init__(self, **kwargs): 
+	def __init__(self, **kwargs):
 		super(MessageBox, self).__init__(**kwargs)
 		self.data = []
-		self.contact_key = '' # for testing purposes, remove
-		global client_instance
-		# global current_convo
-		# print(contact_key)
-		# just for reference
-		# for contact in client_instance.conversation_manager.conversations:
-		# 	friendly_name = client_instance.conversation_manager.conversations[contact].friendly_name
-		# 	self.data.append({'contact_name': friendly_name, 'contact': contact})
-		# temporarily grabbing first contact for testing (errors on no pre-made data)
-		for contact in client_instance.conversation_manager.conversations:
-			self.contact_key = str(client_instance.conversation_manager.conversations[contact].peer)
-			# self.extra_contact_key = str(client_instance.conversation_manager.conversations[contact].peer)
-		# should get contact key from parent
-		# contact_key = self.parent.parent.chosen_contact
-		contact = client_instance.conversation_manager.conversations[self.contact_key]
-		for message_item in contact.messages:
-			self.data.append({'message_text': message_item.message, 'sent': message_item.sent})
-			# print("message text: ", message_item.message)
 		return
-	def set_contact(self, chosen_contact_key = None): # needs massive cleaning up; just for testing
+
+	def set_contact(self, chosen_contact_key): # needs massive cleaning up; just for testing
 		'''sets the current contact to the given (string) key'''
-		self.contact_key = str(chosen_contact_key)
-		# global current_convo
-		contact = client_instance.conversation_manager.conversations[self.contact_key]
+		contact_key = str(chosen_contact_key)
+		contact = client_instance.conversation_manager.conversations[contact_key]
 		self.data = []
 		for message_item in contact.messages:
 			self.data.append({'message_text': message_item.message, 'sent': message_item.sent})
 		return
+
+	def set_target(self, location):
+		return
+	pass
 
 # create different screens
 class HomeScreen(Screen):
 	# def __init__(self, **kwargs):
 	# 	super(Screen, self).__init__(**kwargs)
 	pass
+
 class ComposeScreen(Screen):
 	pass
+
 class BoxScreen(Screen):
 	pass
+
 class ConversationScreen(BoxScreen):
 	# chosen_contact = StringProperty('') # review: might not be necessary, can't remember
 	# ref: https://stackoverflow.com/a/50294037/4513452
@@ -176,13 +165,17 @@ class ConversationScreen(BoxScreen):
 		self.message_box.set_contact(contact)
 		return
 	pass
+
 class InboxScreen(BoxScreen):
 	pass
+
 class OutboxScreen(BoxScreen):
 	pass
+
 class SettingsScreen(Screen):
 	title = StringProperty('') # blank string is default
 	pass
+
 class SetupScreen(Screen):
 	pass
 
